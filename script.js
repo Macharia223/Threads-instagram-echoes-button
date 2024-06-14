246ca6cf455e5448a87dfec1ae2860b4f878c77f
Kevin Macharia <mashakevin19@gmail.com>
2024-06-14 07:51:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const adjustButtonPositionsOnScroll = () => {
-    const initialOffsetVertical = 73.5; // Adjust this value based on your button's top position in vh for vertical layout
-    const additionalOffsetVertical = 86.3; // Adjusted position for cloned button for vertical layout
+    const initialOffsetVertical = 50.5; // Adjust this value based on your button's top position in vh for vertical layout
+    const additionalOffsetVertical = 65.3; // Adjusted position for cloned button for vertical layout
 
     if (scrollContainer.style.overflowX === "auto") {
       // Horizontal layout
